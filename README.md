2e2afd930e1f00f6d1d1fcfc89d05553a2004020
Rob Duarte <me@robduarte.com>
2025-06-01 01:15:24 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,87 @@
+# PackingSlipper
+
+PackingSlipper is a command-line tool for creating a PDF packing slip from a Shopify order.
+
+You probably want to use
+[Shopify's instructions for creating custom CSS for your packing slips](https://help.shopify.com/en/manual/fulfillment/managing-orders/printing-orders/packing-slips/customizing-packing-slips)
+instead of using this. I used my own custom CSS for years so that I could print packing slips on 2x7 Dymo labels
+(which I have way too many of since I started using a 4x6 label printer), but it seems to have started creating
+page breaks in the PDFs that it creates. So I made this. It creates a single packing slip based on the most recent
+order, by default.
+
+## Installing
+
+1. [Get Go](https://go.dev/doc/install)
+2. Run `go install github.com/rahji/packingslipper@latest`
+
+*I would normally create binaries so you can download and run it without installing Go, but I don't expect anyone
+to use this. And if they do, they'll likely want to change something in the code. This was really made just to
+solve my specific problem*
+
+## Configuration
+
+You'll need to create a [Shopify Custom App](https://help.shopify.com/en/manual/apps/app-types#custom-apps) for your
+store and give it permission to view orders. Note the API token that you are given.
+
+### Secrets
+
+The secrets configuration for this project is a bit overkill, but the whole thing is really a learning exercise anyway.
+SOPS is a pretty simple tool for encrypting files. Age is a modern PGP alternative for doing the actual encryption. And
+the Go `go.mozilla.org/sops/v3/decrypt` package provides an easy way to use those encrypted files inside a Go program.
+
+The secrets for this program are the token from above and your Shopify shop name (as shown at the beginning of your Shopify admin site URL).
+These secrets are stored in an encrypted YAML file. Create the YAML file and encrypt it using SOPS. You'll probably
+need to [install SOPS](https://getsops.io/) first. And if you want to use Age for the encryption, as I did, you'll need to
+[install Age](https://github.com/FiloSottile/age) and generate a key pair:
+
+```bash
+mkdir -p ~/.config/sops/age
+age-keygen -o ~/.config/sops/age/keys.txt
+chmod 700 ~/.config/sops/age/keys.txt
+````
+
+Note: If you store your key file in a non-standard directory then you'll want to set an environment variable with its
+location: `export SOPS_AGE_KEY_FILE=/path/to/your/age-key.txt`
+
+Create a `.sops.yaml` file, so SOPS knows what your Age public key is. (It was shown to you by `age-keygen`.)
+
+```yaml
+creation_rules:
+  - age: >-
+      YOUR_PUBLIC_AGE_KEY
+```
+
+Create an unencrypted file called `secrets.enc.yaml`:
+
+```yaml
+api:
+  token: "shpat_..."
+  shop: "your-shop-name"
+```
+
+Then encrypt it, in place: `sops -e -i secrets.enc.yaml`
+
+### Other Config
+
+Edit the included `configuration.yaml` file, according to your needs.
+
+## Usage
+
+Open your terminal application and type `packingslipper`
+
+The program accepts these flags:
+
+| Flag | Default | Description |
+| ---- | ------- | ----------- |
+| outfile | packingslip.pdf | Output PDF filename |
+| offset | 0 | How far back to jump from the most recent order |
+| config | configuration.yaml | Configuration YAML filename |
+| secrets | secrets.enc.yaml | Encrypted secrets YAML filename |
+
+## Issues
+
+Because of the font that I am using, addresses with characters from other languages are not going to work. I tried
+fonts that provide those character sets, but then I went down a rathole of having the program make a guess at
+the language and substitute the correct font. Unfortunately, it seems as though it's not so easy to correctly
+determine whether text is Chinese or Japanese (which seems insane to me).
+
---
+++ main.go
@@ -0,0 +1,207 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/alecthomas/kong"
+	goshopify "github.com/bold-commerce/go-shopify/v4"
+	"github.com/getsops/sops/v3/decrypt"
+	"github.com/signintech/gopdf"
+	"gopkg.in/yaml.v2"
+)
+
+type CLIFlags struct {
+	OutFilename     string `kong:"default='packingslip.pdf',name='outfile',help='Output PDF filename'"`
+	OrderOffset     int    `kong:"default=0,name='offset',help='Offset from most recent order to retrieve'"`
+	ConfigFilename  string `kong:"default='configuration.yaml',name='config',help='Configuration YAML file'"`
+	SecretsFilename string `kong:"default='secrets.enc.yaml',name='secrets',help='Encrypted secrets YAML file'"`
+}
+
+type Config struct {
+	Logo struct {
+		Filename      string `yaml:"filename"`
+		VerticalSpace int    `yaml:"vertical-space"`
+	} `yaml:"logo"`
+
+	Text struct {
+		Salutation string `yaml:"salutation"`
+		Signature  string `yaml:"signature"`
+	} `yaml:"text"`
+}
+
+type Secrets struct {
+	API struct {
+		Token    string `yaml:"token"`
+		ShopName string `yaml:"shop"`
+	} `yaml:"api"`
+}
+
+type AllConfig struct {
+	Config  Config
+	Secrets Secrets
+}
+
+// myPdf embeds gopdf.GoPdf so I can create a WriteLine method later
+// https://stackoverflow.com/questions/28800672/how-to-add-new-methods-to-an-existing-type-in-go
+type myPdf struct {
+	*gopdf.GoPdf
+}
+
+const pageWidth = 144  // points
+const pageHeight = 504 // points
+const lineSpacing = 13
+const fontSize = 10
+
+// createPDF sets up a gopdf.GoPdf document for the packing slip label
+func createPDF() (*myPdf, error) {
+	pdf := &myPdf{&gopdf.GoPdf{}}
+	labelSize := &gopdf.Rect{W: pageWidth, H: pageHeight}
+	pdf.Start(gopdf.Config{PageSize: *labelSize})
+	pdf.AddPage()
+
+	if err := pdf.AddTTFFont("regular", "fonts/arialrounded.ttf"); err != nil {
+		return nil, err
+	}
+
+	if err := pdf.AddTTFFont("bold", "fonts/arialroundedbold.ttf"); err != nil {
+		return nil, err
+	}
+
+	if err := pdf.SetFont("regular", "", fontSize); err != nil {
+		return nil, err
+	}
+
+	return pdf, nil
+}
+
+// writeLine writes a line to the PDF.
+// It wraps long strings at based on pageWidth-rightMargin.
+// More than 1 trailing newline characters are converted to additional line breaks.
+func (p *myPdf) writeLine(s string) {
+	trimmed := strings.TrimRight(s, "\n")
+	newlines := len(s) - len(trimmed)
+
+	// if there is any text after trimming the newlines
+	// then split it at the pageWidth before writing it to a cell
+	if trimmed != "" {
+		texts, _ := p.SplitTextWithWordWrap(trimmed, pageWidth-p.MarginRight())
+		for _, text := range texts {
+			_ = p.Cell(nil, text)
+			p.Br(lineSpacing)
+		}
+	}
+
+	if newlines > 1 {
+		p.Br(lineSpacing * float64(newlines-1))
+	}
+}
+
+func LoadConfig(configPath, secretsPath string) (*AllConfig, error) {
+	// Load plain configuration
+	configData, err := os.ReadFile(configPath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read config file: %w", err)
+	}
+
+	var config Config
+	if err := yaml.Unmarshal(configData, &config); err != nil {
+		return nil, fmt.Errorf("failed to parse config file: %w", err)
+	}
+
+	// Load and decrypt secrets
+	secretsData, err := decrypt.File(secretsPath, "yaml")
+	if err != nil {
+		return nil, fmt.Errorf("failed to decrypt secrets file: %w", err)
+	}
+
+	var secrets Secrets
+	if err := yaml.Unmarshal(secretsData, &secrets); err != nil {
+		return nil, fmt.Errorf("failed to parse secrets file: %w", err)
+	}
+
+	return &AllConfig{
+		Config:  config,
+		Secrets: secrets,
+	}, nil
+}
+
+func main() {
+
+	var cli CLIFlags
+	kong.Parse(&cli)
+
+	// load the configuration
+	cfg, err := LoadConfig(cli.ConfigFilename, cli.SecretsFilename)
+	if err != nil {
+		log.Fatal("Error loading configuration: ", err)
+	}
+
+	// create the blank label
+	p, err := createPDF()
+	if err != nil {
+		log.Fatal("Error creating PDF: ", err)
+	}
+
+	// Create an app somewhere.
+	app := goshopify.App{}
+
+	// Create a new API client (notice the token parameter is the empty string)
+	client, err := goshopify.NewClient(app, cfg.Secrets.API.ShopName, cfg.Secrets.API.Token)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	orders, err := client.Order.List(ctx, goshopify.OrderListOptions{Status: "any"})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// get latest entry
+	latest := orders[cli.OrderOffset]
+
+	p.Image(cfg.Config.Logo.Filename, p.MarginLeft(), p.MarginTop(), nil)
+
+	p.SetXY(p.MarginLeft(), float64(cfg.Config.Logo.VerticalSpace))
+	p.writeLine("Order " + latest.Name)
+	p.writeLine(latest.CreatedAt.Format("Jan 1, 2006") + "\n\n")
+
+	p.SetFont("bold", "", fontSize)
+	p.writeLine("SHIP TO\n")
+
+	p.SetFont("regular", "", fontSize)
+	p.writeLine(latest.ShippingAddress.FirstName + " " + latest.ShippingAddress.LastName)
+	p.writeLine(latest.ShippingAddress.Address1)
+	if latest.ShippingAddress.Address2 != "" {
+		p.writeLine(latest.ShippingAddress.Address2)
+	}
+
+	citystate := strings.Builder{}
+	citystate.WriteString(latest.ShippingAddress.City)
+	citystate.WriteString(" ")
+	citystate.WriteString(latest.ShippingAddress.ProvinceCode)
+	citystate.WriteString(" ")
+	citystate.WriteString(latest.ShippingAddress.Zip)
+	citystate.WriteString("\n")
+	p.writeLine(citystate.String())
+	p.writeLine(latest.ShippingAddress.Country + "\n\n")
+
+	for _, lineItem := range latest.LineItems {
+		p.SetFont("regular", "", fontSize)
+		p.writeLine(fmt.Sprintf("Qty %d", lineItem.Quantity))
+		p.SetFont("bold", "", fontSize)
+		p.writeLine(lineItem.Name)
+		p.SetFont("regular", "", fontSize)
+		p.writeLine("SKU: " + lineItem.SKU + "\n\n")
+	}
+
+	p.writeLine(cfg.Config.Text.Salutation)
+	p.SetFont("bold", "", fontSize)
+	p.writeLine(cfg.Config.Text.Signature)
+
+	p.WritePdf(cli.OutFilename)
+}
